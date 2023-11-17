d48ceaa70865aa2cfdba38acc343db78d19e0fb8
Carmen Biesmans <biesmanscarmen@gmail.com>
2023-11-17 09:56:51 +0100

    Added successful junit test

---
+++ src/test/java/com/switchfully/www/JunitTest.java
@@ -0,0 +1,11 @@
+package com.switchfully.www;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class JunitTest {
+    @Test
+    void successfulTest() {
+        Assertions.assertTrue(true);
+    }
+}
